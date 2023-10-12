414ea4150d9a4ec8627618039d092dc44d19be13
Georg Graf (SPG) <grafg@spengergasse.at>
2023-10-12 15:09:33 +0200

    Folder change

---
+++ 2023-10-09__PLF1_Vorb/PersonTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class PersonTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class PersonTest
+{
+    /**
+     * Default constructor for test class PersonTest
+     */
+    public PersonTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+}
