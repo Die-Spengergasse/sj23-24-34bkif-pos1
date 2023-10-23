1db0c404f14995b5b55f62ccb35cc4b6828387f3
Georg Graf (SPG) <grafg@spengergasse.at>
2023-10-23 02:06:52 +0200

    vscode

---
+++ 2023-10-23__schleifen/Schleife.java
@@ -0,0 +1,10 @@
+public class Schleife {
+    public static void main(String[] args) {
+        System.out.println("sers oida");
+        for (int zeile = 0; zeile < 10; zeile++) {
+            for (int spalte = 0; spalte < 10; spalte++)
+                System.out.print(zeile + " " + spalte + " | ");
+            System.out.println();
+        }
+    }
+}
