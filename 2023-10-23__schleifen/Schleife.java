f770dd0ac22c02564003daf3b8c9ac44fee3dddc
Georg Graf (SPG) <grafg@spengergasse.at>
2023-10-23 11:04:57 +0200

    fib mit overflow

--- 2023-10-23__schleifen/Schleife.java
+++ 2023-10-23__schleifen/Schleife.java
@@ -1,11 +1,13 @@
 public class Schleife {
     public static void main(String[] args) {
         System.out.println();
-        for (int zeile = 0; zeile < 10; zeile++) {
-            for (int spalte = 0; spalte < 10; spalte++)
-                System.out.print(zeile + " " + spalte + " | ");
-            System.out.println();
+        long prepre = 0;
+        long pre = 1;
+
+        for (long fib = pre + prepre; fib > 0; fib = pre + prepre) {
+            System.out.println(fib);
+            prepre = pre;
+            pre = fib;
         }
-        System.out.println();
     }
 }
