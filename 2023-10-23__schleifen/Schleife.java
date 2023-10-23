41bc4a3d7ebcb915d80bc20a71560681849b4574
Georg Graf (SPG) <grafg@spengergasse.at>
2023-10-23 17:09:59 +0200

    Schleife aktuell

--- 2023-10-23__schleifen/Schleife.java
+++ 2023-10-23__schleifen/Schleife.java
@@ -1,13 +1,21 @@
-public class Schleife {
-    public static void main(String[] args) {
-        System.out.println();
-        long prepre = 0;
-        long pre = 1;
+/* Angabe Fizz - Buzz
+Schleife von 0 bis 25
+Modulo Operator
+Wenn Zahl teilbar durch 3: Ausgabe "FIZZ"
+Wenn teilbar durch 5: Ausgabe "BUZZ"
+Wenn teilbar durch 3 und 5 "FIZZ-BUZZ"
+Ansonsten: Zahl
+ */
 
-        for (long fib = pre + prepre; fib > 0; fib = pre + prepre) {
-            System.out.println(fib);
-            prepre = pre;
-            pre = fib;
+public class Schleife {
+    public static void main(String[] args) {  // Hallo Welt ich bin da
+        int i;
+        for (i = 0; i < 10; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println("In Schleife, i ist: " + i);
         }
+        System.out.println("Nach Schleife, i ist: " + i);
     }
 }
