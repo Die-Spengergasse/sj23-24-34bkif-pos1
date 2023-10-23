99782ebd588f572ae5a0bd0356d118c69654b0cb
Georg Graf (SPG) <grafg@spengergasse.at>
2023-10-23 02:08:25 +0200

    fix

--- 2023-10-23__schleifen/Schleife.java
+++ 2023-10-23__schleifen/Schleife.java
@@ -1,10 +1,11 @@
 public class Schleife {
     public static void main(String[] args) {
-        System.out.println("sers oida");
+        System.out.println();
         for (int zeile = 0; zeile < 10; zeile++) {
             for (int spalte = 0; spalte < 10; spalte++)
                 System.out.print(zeile + " " + spalte + " | ");
             System.out.println();
         }
+        System.out.println();
     }
 }
