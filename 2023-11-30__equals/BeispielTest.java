c57dd76822c9015bb28f72de3a7c3eb8883e96a5
Georg Graf (SPG) <grafg@spengergasse.at>
2023-12-04 11:41:28 +0100

    m

---
+++ 2023-11-30__equals/BeispielTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeispielTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStadt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setStadt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEinwohner() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEinwohner() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetStadt() {
+    }
+}
