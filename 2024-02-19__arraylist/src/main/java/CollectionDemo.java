7600b54ac99a67518c1f51772ab5a2d1b8f3370b
Georg Graf (SPG) <grafg@spengergasse.at>
2024-02-19 17:51:26 +0100

    autocommit

--- 2024-02-19__arraylist/src/main/java/CollectionDemo.java
+++ 2024-02-19__arraylist/src/main/java/CollectionDemo.java
@@ -1,18 +1,20 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class CollectionDemo {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(7);
-        list.add(8);
-        list.add(5);
-        list.add(3);
-        list.add(2);
-        list.add(-7);
+        ArrayList<Integer> list = new ArrayList<>();
+        Random rnd = new Random();
+        for (int i = 0; i<21; i++) {
+            list.add (rnd.nextInt(199)-99);
+        }
+        // list.add(null);
         System.out.println(list);
+        System.out.println(list.size());
         list.sort(new SortierKriterium());
+        // zkukunftsmusik list.sort((a,b) -> b-a);
         System.out.println(list);
 
     }
--- 2024-02-19__arraylist/src/main/java/SortierKriterium.java
+++ 2024-02-19__arraylist/src/main/java/SortierKriterium.java
@@ -4,7 +4,26 @@ public class SortierKriterium implements Comparator<Integer> {
 
     @Override
     public int compare(Integer o1, Integer o2) {
-        System.out.println("vergleiche " + o1 + " mit " + o2);
-        return o2-o1;
+        System.out.print("vergleiche " + o1 + " mit " + o2 + " -> ");
+        boolean o1_gerade = o1 % 2 == 0;
+        boolean o2_gerade = o2 % 2 == 0;
+        if (o1_gerade && o2_gerade) {
+            // beide gerade
+            System.out.println(o1 - o2);
+            return o1 - o2;
+        }
+        if (!o1_gerade && !o2_gerade) {
+            // beide ungerade
+            System.out.println(o2 - o1);
+            return o2 - o1;
+        }
+        if (o1_gerade) {
+            // o1 gerade UND o2 ungerade
+            System.out.println(-1);
+            return -1;
+        }
+        // o2 gerade und o1 ist ungerade
+        System.out.println("+1");
+        return 1;
     }
 }
