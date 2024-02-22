30c910475c2b39340da1bc8e8cdbf3687dc95194
Georg Graf (SPG) <grafg@spengergasse.at>
2024-02-22 11:30:47 +0100

    autocommit

---
+++ 2024-02-22__csv/js/fakecsv.js
@@ -0,0 +1,11 @@
+const { fakerDE, fakerEN } = require('@faker-js/faker');
+
+for (let i = 0; i < 1000; i++) {
+    const sex = fakerEN.person.sex();
+    const fullName = fakerDE.person.fullName({ sex: sex });
+    console.log(
+        `${fakerDE.string.uuid()};${fullName};${sex};${
+            fakerDE.number.int({ min: 100, max: 235 }) / 100
+        };${fakerDE.number.int({ min: 35, max: 170 })}`
+    );
+}
