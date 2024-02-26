16ce956c628b5c0dffb0155183af3225b2e391d3
Georg Graf (SPG) <grafg@spengergasse.at>
2024-02-26 16:09:57 +0100

    autocommit

--- 2024-02-22__csv/src/main/java/PersonSort.java
+++ 2024-02-22__csv/src/main/java/PersonSort.java
@@ -1,18 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class PersonSort {
-    public static void main(String[] args) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-
-            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-                System.out.println(line);
-            }
-            reader.close();
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("ARG 0: infile, ARG 1: outfile");
+        }
+        ArrayList<Person> personenArrayList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            personenArrayList.add(new Person(line));  // TODO hier könnten Exeptions passieren
+        }
+        reader.close();
+        personenArrayList.sort(null);
+        for (Person p: personenArrayList) {
+            // outfile.write(p.toString())
         }
     }
 }
+
