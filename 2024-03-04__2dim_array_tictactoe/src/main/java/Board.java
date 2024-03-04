eed038e8ab9a723cc8d1c06532c0c924b152781b
Georg Graf (SPG) <grafg@spengergasse.at>
2024-03-04 17:53:11 +0100

    autocommit

--- 2024-03-04__2dim_array_tictactoe/src/main/java/Board.java
+++ 2024-03-04__2dim_array_tictactoe/src/main/java/Board.java
@@ -9,12 +9,26 @@ public class Board {
     }
 
     public void placePiece(int place, boolean player) {
-        int zeile = (place - 1) / 3;
-        int spalte = (place - 1) % 3;
+        int zeile = getZeile(place);
+        int spalte = getSpalte(place);
         if (fields[zeile][spalte] != null) {
             throw new IllegalArgumentException("Da ist schon was: " + (fields[zeile][spalte] ? "X" : "O"));
         }
         fields[zeile][spalte] = player;
+    }
+
+    private int getZeile(int place) {
+        if (place < 1 || place > 9) {
+            throw new IllegalArgumentException("invalid place: " + place);
+        }
+        return (place - 1) / 3;
+    }
+
+    private int getSpalte(int place) {
+        if (place < 1 || place > 9) {
+            throw new IllegalArgumentException("invalid place: " + place);
+        }
+        return (place - 1) % 3;
     }
 
     @Override
@@ -26,8 +40,9 @@ public class Board {
             for (int spalte = 0; spalte < 3; spalte++) {
                 rw += getSymbol(zeile, spalte) + "|";
             }
-            rw += "\n" + leerZeile;
+            rw += "\n";
         }
+        rw += leerZeile;
         return rw;
     }
 
@@ -38,4 +53,66 @@ public class Board {
         return fields[zeile][spalte] ? "X" : "O";
     }
 
+    public boolean isFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (fields[i][j] == null) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    public boolean checkWinner(boolean player) {
+        // in allen Zeilen suchen
+        for (int i = 0; i < 3; i++) {
+            boolean alleInZeile = true;  // gehe davon in dieser sind alle als "player" besetz
+            for (int j = 0; i < 3; j++) {  // spalten
+                if (fields[i][j] == null || fields[i][j] != player) {
+                    alleInZeile = false;
+                    break;
+                }
+            }
+            if (alleInZeile) {
+                return true;
+            }
+        }
+        // allen spalten suchen
+        for (int j = 0; j < 3; j++) {
+            boolean alleInSpalte = true;
+            for (int i = 0; i < 3; i++) {
+                if (fields[i][j] == null || fields[i][j] != player) {
+                    alleInSpalte = false;
+                    break;
+                }
+            }
+            if (alleInSpalte) {
+                return true;
+            }
+        }
+        boolean alleInDiagonale = true;
+        for (int i = 0; i < 3; i++) {
+            if (fields[i][i] == null || fields[i][i] != player) {
+                alleInDiagonale = false;
+                break;
+            }
+        }
+        if (alleInDiagonale) {
+            return true;
+        }
+        alleInDiagonale = true;
+        for (int i = 0; i < 3; i++) {
+            int j = 2 - i;
+            if (fields[i][j] == null || fields[i][j] != player) {
+                alleInDiagonale = false;
+                break;
+            }
+        }
+        if (alleInDiagonale) {
+            return true;
+        }
+        return false;
+    }
 }
--- 2024-03-04__2dim_array_tictactoe/src/main/java/TicTacToe.java
+++ 2024-03-04__2dim_array_tictactoe/src/main/java/TicTacToe.java
@@ -1,87 +1,51 @@
-import javax.swing.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
-    static ArrayList<Integer> playerPositions = new ArrayList<>();
-    static ArrayList<Integer> cpuPositions = new ArrayList<>();
-
     public static void main(String[] args) {
-    Board gameboard = new Board();
+        Board gameboard = new Board();
         Scanner scan = new Scanner(System.in);
-
+        Random rand = new Random();
         while (true) {
-            System.out.println("Enter your placement (1-9):");
-
-            int playerPos = scan.nextInt();
-            while (playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)) {
-                System.out.println("position taken! enter correct Position");
-                playerPos = scan.nextInt();
+            while (true) {
+                try {
+                    System.out.println("Enter your placement (1-9):");
+                    int playerPos = scan.nextInt();
+                    gameboard.placePiece(playerPos, true);
+                    break;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
             }
-
-            gameboard.placePiece(playerPos, true);
-
-            Random rand = new Random();
-            int cpuPos = rand.nextInt(9) + 1;
-            while (playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)) {
-                cpuPos = rand.nextInt(9) + 1;
-            }
-
-            gameboard.placePiece( cpuPos, false);
-
             System.out.println(gameboard);
-
-            String result = checkWinner();
-            System.out.println(result);
-            if (result.contains("win") || result.equals("It's a tie! :/")) {
+            if (gameboard.checkWinner(true)) {
+                System.out.println("Player wins");
+                break;
+                // System.exit(0);
+            }
+            // CPU's turn
+            if (gameboard.isFull()) {
+                System.out.println("Both win, board is full");
+                break;
+            }
+            while (true) {
+                try {
+                    gameboard.placePiece(rand.nextInt(9) + 1, false);
+                    break;
+                } catch (Exception e) {
+                }
+            }
+            System.out.println(gameboard);
+            if (gameboard.checkWinner(false)) {
+                System.out.println("CPU wins");
+                break;
+                // System.exit(0);
+            }
+            if (gameboard.isFull()) {
+                System.out.println("Both win, board is full");
                 break;
             }
         }
     }
-
-    public static void printGameBoard(char[][] gameboard) {
-        for (char[] row : gameboard) {
-            for (char c : row) {
-                System.out.print(c);
-            }
-            System.out.println();
-        }
-    }
-
- 
-    public static String checkWinner() {
-        List topRow = Arrays.asList(1, 2, 3);
-        List midRow = Arrays.asList(4, 5, 6);
-        List botRow = Arrays.asList(7, 8, 9);
-        List topCol = Arrays.asList(1, 4, 7);
-        List midCol = Arrays.asList(2, 5, 8);
-        List botCol = Arrays.asList(3, 6, 9);
-        List rightdiagonal = Arrays.asList(1, 5, 9);
-        List leftdiagonal = Arrays.asList(3, 5, 7);
-
-        List<List> winning = new ArrayList<List>();
-        winning.add(topRow);
-        winning.add(midRow);
-        winning.add(botRow);
-        winning.add(topCol);
-        winning.add(midCol);
-        winning.add(botCol);
-        winning.add(rightdiagonal);
-        winning.add(leftdiagonal);
-
-        for (List l : winning) {
-
-            if (cpuPositions.contains(l)) {
-                return "Sorry, CPU wins! :(";
-            } else if (playerPositions.containsAll(l)) {
-                return "Congratulations! You win! :)";
-            } else if (playerPositions.size() + cpuPositions.size() == 9) {
-                return "It's a tie! :/";
-            }
-        }
-        return "";
-    }
-
-   /* public static String tryAgain() {
-
-    }*/
 }
