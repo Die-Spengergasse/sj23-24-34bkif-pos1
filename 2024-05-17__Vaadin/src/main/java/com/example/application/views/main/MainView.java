5c3150ecca3a2443fb11de9a1ae4665103a39c91
Georg Graf (SPG) <grafg@spengergasse.at>
2024-06-06 18:06:05 +0200

    autocommit

---
+++ 2024-05-17__Vaadin/src/main/java/com/example/application/views/main/MainView.java
@@ -0,0 +1,34 @@
+package com.example.application.views.main;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Main")
+@Route(value = "")
+@RouteAlias(value = "")
+public class MainView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public MainView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
